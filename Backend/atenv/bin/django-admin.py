1c61b4436adc5bb973248822a9317a02b2fd59ab
Godswill <30760648+WillDera@users.noreply.github.com>
2021-07-08 21:23:06 +0100

    edit

---
+++ Backend/atenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/will/Documents/Projects/Python_projects/AttendanceApp/Backend/atenv/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
